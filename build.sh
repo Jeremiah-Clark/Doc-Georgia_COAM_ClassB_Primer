833afb0b17e8c152dbf22105c554faf676cbb67a
Jeremiah Clark <62705464+Jeremiah-Clark@users.noreply.github.com>
2026-02-10 14:59:55 -0500

    Update build.sh with corrected file names

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pandoc master.yaml \
        00-Frontmatter.md \
        01-Basics_of_COAM.md \
        02-Other_Design_Considerations.md \
-       03-Government_Document_Excerpts \
+       03-Government_Document_Excerpts.md \
        -o output.pdf
 
 echo "PDF generated: output.pdf"
