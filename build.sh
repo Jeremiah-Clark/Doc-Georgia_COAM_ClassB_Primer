af7be6160f112be007cf4ca267e46ad68b02fc41
Jeremiah Clark <62705464+Jeremiah-Clark@users.noreply.github.com>
2026-02-10 15:44:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ pandoc master.yaml \
        00-Frontmatter.md \
        01-Basics_of_COAM.md \
        02-Other_Design_Considerations.md \
-       -o output.pdf
+       -o Georgia_COAM_ClassB_Primer-Latest.pdf
 
 echo "PDF generated: output.pdf"
