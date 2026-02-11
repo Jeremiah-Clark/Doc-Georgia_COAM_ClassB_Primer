0b8eef2a7de07eef036076d0894a17c07c7ecb2d
Jeremiah Clark <62705464+Jeremiah-Clark@users.noreply.github.com>
2026-02-11 00:07:23 -0500

    Update build.sh - Updated name of 02

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 pandoc master.yaml \
        00-Frontmatter.md \
        01-Basics_of_COAM.md \
-       02-Other_Design_Considerations.md \
+       02-Other_Considerations.md \
        --pdf-engine=xelatex \
        -o Georgia_COAM_ClassB_Primer-Latest.pdf
 
