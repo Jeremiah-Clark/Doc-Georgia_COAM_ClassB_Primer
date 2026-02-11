ed254dec9e6bc5010ac761760f49de4f94f06542
Jeremiah Clark <62705464+Jeremiah-Clark@users.noreply.github.com>
2026-02-11 16:23:04 -0500

    Update testbuild.sh

--- testbuild.sh
+++ testbuild.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-pandoc master.yaml \
-       test.md \
-       --pdf-engine=xelatex \
-       --lua-filter=callouts.lua \
-       -o Georgia_COAM_ClassB_Primer-Latest.pdf
-
-echo "PDF generated: Georgia_COAM_ClassB_Primer-Latest.pdf"
+pandoc master.yaml test.md --pdf-engine=xelatex --lua-filter=callouts.lua -o test.pdf
