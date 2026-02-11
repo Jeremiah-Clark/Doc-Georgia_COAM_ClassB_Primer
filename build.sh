486eb468351aa3b2e3757844ef2d536af3319043
Jeremiah Clark <62705464+Jeremiah-Clark@users.noreply.github.com>
2026-02-11 16:47:46 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ pandoc master.yaml \
        01-Basics_of_COAM.md \
        02-Other_Considerations.md \
        --pdf-engine=xelatex \
-       --lua-filter=callouts.lua \
+       --lua-filter=divs-to-env.lua \
        -o Georgia_COAM_ClassB_Primer-Latest.pdf
 
 echo "PDF generated: Georgia_COAM_ClassB_Primer-Latest.pdf"
