a34e53bc8970c1e92aac198b41f1588524e44c0a
Jeremiah Clark <jc@jeremiahclark.com>
2026-02-11 21:39:18 -0500

    Updated build file with new 02 name

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 pandoc master.yaml \
        00-Frontmatter.md \
        01-Basics_of_COAM.md \
-       02-Other_Considerations.md \
+       02-Market_Considerations.md \
        --pdf-engine=xelatex \
        --lua-filter=divs-to-env.lua \
        -o Georgia_COAM_ClassB_Primer-Latest.pdf
