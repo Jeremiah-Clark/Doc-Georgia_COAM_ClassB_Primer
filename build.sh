c8c8d454e4c1a34f3a269a77e449a7506dd03fbf
Jeremiah Clark <jc@jeremiahclark.com>
2026-02-11 21:55:05 -0500

    Udated build file with new numbering

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 pandoc master.yaml \
        00-Frontmatter.md \
        01-COAM_Regulations.md \
-       02-Market_Considerations.md \
+       02-Skill_Test_Considerations.md \
+       03-Market_Considerations.md \
        --pdf-engine=xelatex \
        --lua-filter=divs-to-env.lua \
        -o Georgia_COAM_ClassB_Primer-Latest.pdf
